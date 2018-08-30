53293316097a20c0913d2d48619a3ecd33f7ae43
Kirk Dorsey <dorsey_k17@hotmail.com>
2018-08-29 21:06:48 -0500

    Added BeerSong and HelloWorld java files

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Let's Play
+public class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Why do you think you're all that and a bag of chips?");
+		System.out.println("Because I'm Batman!");
+	}
+}
+

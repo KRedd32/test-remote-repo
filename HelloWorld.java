f1047d3011c1766f71ab6e87bbbc355b39560af9
Kirk Dorsey <dorsey_k17@hotmail.com>
2019-04-29 21:57:13 -0500

    Updated HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Why do you think you're all that and a bag of chips?");
 		System.out.println("Because I'm Batman!");
+		System.out.println("Guess that explains it!");
 	}
 }
 
